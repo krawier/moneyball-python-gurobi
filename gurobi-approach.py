d40dde3fd2f0ec4fcd5d4a586399f41d6840ff81
Oliwier K <o.krawczyk06@gmail.com>
2026-05-24 15:45:51 +0200

    feat: add printer

--- gurobi-approach.py
+++ gurobi-approach.py
@@ -1,5 +1,6 @@
 from gurobipy import *
 from data import *
+import pandas as pd
 
 B_idx = df_batters.index.tolist()
 P_idx = df_pitchers.index.tolist()
@@ -34,3 +35,40 @@ problem.addConstr(quicksum(x_batters[i] for i in idx_SS) == 1, "Exactly_1_Shorts
 problem.addConstr(quicksum(x_batters[i] for i in idx_OF) == 3, "Exactly_3_Outfielders")
 
 problem.addConstr(quicksum(x_pitchers[i] for i in idx_SP) == 1, "Exactly_1_Starting_Pitcher")
+
+problem.optimize()
+
+if problem.status == GRB.OPTIMAL:
+    print("\n" + "="*50)
+    print("OPTIMAL ROOSTER")
+    print("="*50)
+    print(f"Total team value: {problem.objVal:.2f}")
+    print("-" * 50)
+
+    selected_players = []
+    total_cost = 0
+
+    for i in P_idx:
+        if x_pitchers[i].x > 0.5:
+            name, team, pos, val, sal = df_pitchers.loc[i, 'Name'], df_pitchers.loc[i, 'Team'], df_pitchers.loc[i, 'Pos'], df_pitchers.loc[i, 'Value'], df_pitchers.loc[i, 'Salary']
+            total_cost += sal
+            selected_players.append({'Position': pos, 'Name': name, 'Team': team, 'Value': val, 'Salary': sal})
+
+    for i in B_idx:
+        if x_batters[i].x > 0.5:
+            name, team, pos, val, sal = df_batters.loc[i, 'Name'], df_batters.loc[i, 'Team'], df_batters.loc[i, 'Pos'], df_batters.loc[i, 'Value'], df_batters.loc[i, 'Salary']
+            total_cost += sal
+            selected_players.append({'Position': pos, 'Name': name, 'Team': team, 'Value': val, 'Salary': sal})
+
+    for p in selected_players:
+        print(f"[{p['Position']:<2}] {p['Name']:<20} ({p['Team']:<3}) | Value: {p['Value']:>6.2f} | Koszt: ${p['Salary']}")
+
+    print("-" * 50)
+    print(f"Used budget: ${total_cost} / ${totalBudget}\n")
+
+    results_df = pd.DataFrame(selected_players)
+
+    results_df.to_csv('gurobi_results.csv', index=False)
+    print("Resutls saved to 'gurobi_results.csv' ")
+else:
+    print("Model didn't find an optimal solution. Budget too small, or constraints too rigorious!")
